184c8772e5e28836edc3438e15c3502cab931236
Sam M <emberdesignltd@protonmail.com>
2021-06-03 15:54:30 +0100

    feat(Title): Add title module/comp and refactor existing comps to utilise new title comp

--- src/app/user-posts/user-posts.module.ts
+++ src/app/user-posts/user-posts.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TitleModule } from './../title/title.module';
 import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [PostComponent, PostsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TitleModule],
   exports: [PostsComponent],
 })
 export class UserPostsModule {}
--- src/app/word-frequency/word-frequency.module.ts
+++ src/app/word-frequency/word-frequency.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TitleModule } from './../title/title.module';
 import { WordFrequencyHistogramComponent } from './word-frequency-histogram/word-frequency-histogram.component';
 import { WordFrequencyTableComponent } from './word-frequency-table/word-frequency-table.component';
 
 @NgModule({
   declarations: [WordFrequencyTableComponent, WordFrequencyHistogramComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TitleModule],
   exports: [WordFrequencyHistogramComponent, WordFrequencyTableComponent],
 })
 export class WordFrequencyModule {}
